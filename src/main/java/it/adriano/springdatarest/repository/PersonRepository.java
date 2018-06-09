931ba3fab319d022a529ef99f7a432a80fadf9fd
Adriano91e <adriano.enea@gmail.com>
2018-06-09 20:05:51 +0200

    update repository

--- src/main/java/it/adriano/springdatarest/repository/PersonRepository.java
+++ src/main/java/it/adriano/springdatarest/repository/PersonRepository.java
@@ -1,11 +1,17 @@
 package it.adriano.springdatarest.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import it.adriano.springdatarest.model.Person;
 
-public interface PersonRepository extends CrudRepository<Person,Integer>{
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends CrudRepository<Person, Integer> {
 	
-
-
+	List<Person> findByFirstName(@Param(value = "name") String name);
+		
 }
